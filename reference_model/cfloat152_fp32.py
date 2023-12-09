f8f645fe9d3eb79b3f4e2fbd47fcfc713e5e8fa8
RohitSrinivasRG <rohit@mindgrovetech.in>
2023-12-09 17:03:59 +0530

    started for vice versa conversion

--- reference_model/cfloat152_fp32.py
+++ reference_model/cfloat152_fp32.py
@@ -6,60 +6,21 @@ import sys
 import logging
 
 
-# Function for converting decimal to binary
-def float_bin(my_number, places = 3): 
-    my_whole, my_dec = str(my_number).split(".")
-    my_whole = int(my_whole)
-    res = (str(bin(my_whole))+".").replace('0b','')
- 
-    for x in range(places):
-        my_dec = str('0.')+str(my_dec)
-        temp = '%1.20f' %(float(my_dec)*2)
-        my_whole, my_dec = temp.split(".")
-        res += my_whole
-    return res
+import struct
 
-def fp32_bin (n):
-    # identifying whether the number
-    # is positive or negative
-    sign = 0
-    if n < 0 : 
-        sign = 1
-        n = n * (-1) 
-    p = 30
-    # convert float to binary
-    dec = float_bin (n, places = p)
- 
-    dotPlace = dec.find('.')
-    onePlace = dec.find('1')
-    # finding the mantissa
-    if onePlace > dotPlace:
-        dec = dec.replace(".","")
-        onePlace -= 1
-        dotPlace -= 1
-    elif onePlace < dotPlace:
-        dec = dec.replace(".","")
-        dotPlace -= 1
-    mantissa = dec[onePlace+1:]
- 
-    # calculating the exponent(E)
-    exponent = dotPlace - onePlace
-    exponent_bits = exponent + 127
- 
-    # converting the exponent from
-    # decimal to binary
-    exponent_bits = bin(exponent_bits).replace("0b",'') 
- 
-    mantissa = mantissa[0:23]
- 
-    # the IEEE754 notation in binary     
-    final = str(sign) + exponent_bits.zfill(8) + mantissa
- 
-    # convert the binary to hexadecimal 
-    hstr = '0x%0*X' %((len(final) + 3) // 4, int(final, 2)) 
-    return final
+def fp32_bin(value):
+    # Use struct.pack to convert the float to IEEE 754 binary representation
+    binary_representation = struct.pack('>f', value)
 
-def convert_cfloat152_fp32(cfloat152_in,bias,bin_ret):
+    # Convert binary to a list of 32 bits (0s and 1s)
+    bits = [format(byte, '08b') for byte in binary_representation]
+
+    # Combine the bits into a single string
+    binary_string = ''.join(bits)
+
+    return binary_string
+
+def convert_cfloat152_fp32(cfloat152_in,bias):
     dict_n     = {}
     dict_d     = {}
     CHECK_RES  = False
@@ -110,8 +71,9 @@ def convert_cfloat152_fp32(cfloat152_in,bias,bin_ret):
     else:
         logging.error("The given cfloat number is not a valid input")
     
-    if (bin_ret == 1 and CHECK_RES):
-        print(fp32_bin(cfloat152_in))
+    if (CHECK_RES):
+        bin_fp32 = fp32_bin(cfloat152_in)
+        return bin_fp32
     else:
         sys.exit(1)
 
